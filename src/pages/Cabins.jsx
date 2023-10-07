b6ad432d78674d923f087f5df936b8db30f4d60b
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-10-07 16:53:36 +0530

    added supabase client

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
+      <img
+        src="https://rcizndcjqludlczfwbrk.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-10-07T11%3A18%3A19.424Z"
+        alt=""
+      />
       <p>TEST</p>
     </Row>
   );
