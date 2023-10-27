507bc554ed9bfe2387153c5f9db3492edc1ca462
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-10-27 18:10:51 +0530

    Added functionality to view booking details and check in page

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getBooking } from "../../services/apiBookings";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+
+  const {
+    data: booking,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["booking", bookingId],
+    queryFn: () => getBooking(bookingId),
+    retry: false,
+  });
+
+  return { booking, isLoading, error };
+}
