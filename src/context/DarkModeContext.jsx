17aa68d607a0f94390c479f8091442d191d5b601
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-10-29 14:34:07 +0530

    Added functionality to toggle between light and dark mode using a custom dark-mode context

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.remove("light-mode");
+        document.documentElement.classList.add("dark-mode");
+        return;
+      }
+
+      document.documentElement.classList.remove("dark-mode");
+      document.documentElement.classList.add("light-mode");
+    },
+    [isDarkMode]
+  );
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (!context)
+    throw new Error(
+      "Dark Mode Context was used outside of the DarkModeProvider"
+    );
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
+import DarkModeToggle from "./DarkModeToggle";
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
@@ -18,6 +19,9 @@ function HeaderMenu() {
         <ButtonIcon onClick={() => navigate("/account")}>
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
